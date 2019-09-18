e526f2dcbee205a845eb71ed29d3e5dd0e91e4e8
fpin3k <1104829018@qq.com>
2019-09-18 16:44:17 +0800

    add header

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="navbar navbar-default topnav">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="/" class="navbar-brand">
+          <span class="title">{{ logo.title }}</span>
+          <img :src="logo.src" :alt="logo.title">
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+  data() {
+    return {
+      logo: {
+        src: `${this.uploadsUrl}communities/hIZjRRdF8oVYZy69XJnT.png`,
+
+        title: 'VuejsCaff'
+      }
+    }
+  },
+  beforeCreate() {
+    this.uploadsUrl = 'https://cdn.learnku.com/uploads/'
+  }
+}
+</script>
+
+<style scoped>
+.title { display: none;}
+</style>
